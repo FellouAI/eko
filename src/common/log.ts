856189525a7188dec708e8a19b1b03fd8802ac32
Jamie <3299127392@qq.com>
2025-03-04 01:13:33 +0800

    feat: add log storage and improve recorder instance retrieval - implement the getAllLogs method to return stored logs in array format. - enhance the logging method factory by storing truncated logs in logStorage. bug: the toReadableString method does not handle nested objects well and cannot achieve the expected effect.

--- src/common/log.ts
+++ src/common/log.ts
@@ -2,17 +2,19 @@ import log from 'loglevel';
 
 export class EkoLogger {
   private logger: log.Logger;
+  private logStorage: string[] = [];
 
   constructor() {
     this.logger = log.getLogger('EkoLogger');
     this.logger.setLevel(log.levels.TRACE);
 
     const originalFactory = this.logger.methodFactory;
-    
+
     this.logger.methodFactory = (methodName, logLevel, loggerName) => {
       const rawMethod = originalFactory(methodName, logLevel, loggerName);
       return (...args: any[]) => {
         const truncatedArgs = args.map(arg => this.toReadableString(arg));
+        this.logStorage.push(`[${methodName.toUpperCase()}] ${truncatedArgs.join(' ')}`);
         rawMethod(...truncatedArgs);
       };
     };
@@ -29,9 +31,13 @@ export class EkoLogger {
     }
   }
 
-  public getLogger(): log.Logger {
+  public getLoggerInstance(): log.Logger {
     return this.logger;
+  }
+
+  public getAllLogs(): string[] {
+    return this.logStorage;
   }
 }
 
-export const logger = new EkoLogger().getLogger();
+export const logger = new EkoLogger().getLoggerInstance();
