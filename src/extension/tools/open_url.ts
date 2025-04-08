b23297e8d99bfce83d11b14d69ad92d68eb7180d
HairlessVillager <hairlessvillager@foxmail.com>
2025-04-08 20:47:05 +0800

    feat: replace `browser_use`, `open_url` return value with screenshot

--- src/extension/tools/open_url.ts
+++ src/extension/tools/open_url.ts
@@ -1,16 +1,18 @@
-import { OpenUrlParam, OpenUrlResult } from '../../types/tools.types';
+import { BrowserUseParam, OpenUrlParam, OpenUrlResult } from '../../types/tools.types';
 import { Tool, InputSchema, ExecutionContext } from '../../types/action.types';
 import { getWindowId, open_new_tab } from '../utils';
+import { ToolReturnsScreenshot } from './tool_returns_screenshot';
 
 /**
  * Open Url
  */
-export class OpenUrl implements Tool<OpenUrlParam, OpenUrlResult> {
+export class OpenUrl extends ToolReturnsScreenshot<OpenUrlParam> {
   name: string;
   description: string;
   input_schema: InputSchema;
 
   constructor() {
+    super();
     this.name = 'open_url';
     this.description = 'Open the specified URL link in browser window';
     this.input_schema = {
@@ -35,7 +37,7 @@ export class OpenUrl implements Tool<OpenUrlParam, OpenUrlResult> {
    * @param {*} params { url: 'https://www.google.com', newWindow: true }
    * @returns > { tabId, windowId, title, success: true }
    */
-  async execute(context: ExecutionContext, params: OpenUrlParam): Promise<OpenUrlResult> {
+  async realExecute(context: ExecutionContext, params: OpenUrlParam): Promise<OpenUrlResult> {
     console.log('Starting execute function with context:', context, 'and params:', params);
 
     // 参数验证
